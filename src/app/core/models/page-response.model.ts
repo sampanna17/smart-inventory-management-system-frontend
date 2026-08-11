e7de5ece03e3e4c63609ab69bd305906f7574fef
sampanna17 <sampannapiya13@gmail.com>
2026-08-11 10:32:42 +0545

    Product page pagination

---
+++ src/app/core/models/page-response.model.ts
@@ -0,0 +1,11 @@
+export interface PageResponse<T> {
+  content: T[];
+  pageNumber: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  last: boolean;
+  hasNext: boolean;
+  hasPrevious: boolean;
+}
