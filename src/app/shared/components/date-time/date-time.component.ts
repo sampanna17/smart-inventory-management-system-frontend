76cb753dfe305ce379f98b1d6903d6a4b423e6a9
sampanna17 <sampannapiya13@gmail.com>
2026-08-14 15:21:18 +0545

    Corrected UI Design and font sized

--- src/app/shared/components/date-time/date-time.component.ts
+++ src/app/shared/components/date-time/date-time.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, computed } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroCalendarDays, heroClock } from '@ng-icons/heroicons/outline';
